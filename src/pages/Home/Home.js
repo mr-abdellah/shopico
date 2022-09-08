c7452a0523d33a57c217a4b2e694a34e042e586e
Abdellah Belkaid <belkaid.abdulah@gmail.com>
2022-09-08 09:39:12 +0100

    Add responsive hero section for home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,37 +1,12 @@
-import React, { useEffect } from "react";
-import "./Home.css";
-import { Link } from "react-router-dom";
-import { useGetBestSellerQuery } from "../../redux/products";
+import React from 'react'
+import Hero from './../../components/Hero/Hero';
 
 const Home = () => {
-  const bestProducts = useGetBestSellerQuery();
-  useEffect(() => {
-    bestProducts.isSuccess
-      ? console.log(bestProducts.data)
-      : console.log("khesara");
-  }, []);
-
-  const random = Math.floor(Math.random() * 30);
-
-  const productOne = bestProducts.isSuccess? bestProducts?.data[random].product_main_image_url: '';
   return (
-    <div className="home">
-      <div className="home-container">
-        <h1>
-          Hello! What would <br /> you like to buy?
-        </h1>
-        <p>
-          Search between millions of products from hundreads different online
-          shops. Our price comparison service helps you find the best deal on
-          the market.
-        </p>
-        <Link id="shop-now-link" to="/best_deals">
-          Shop now
-        </Link>
-        <img src={productOne} alt="" />
-      </div>
-    </div>
-  );
-};
+      <>
+      <Hero />
+      </>
+  )
+}
 
-export default Home;
+export default Home
